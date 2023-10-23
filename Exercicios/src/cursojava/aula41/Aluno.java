b8b1a760e600cfa3ac82ae76396838b95d5b8674
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2023-10-23 08:53:35 -0300

    Acrescentado Exercício de Classe Abstract

---
+++ Exercicios/src/cursojava/aula41/Aluno.java
@@ -0,0 +1,87 @@
+package cursojava.aula41;
+
+public class Aluno extends Pessoa {
+    
+    private String cursos;
+    private double[] notas;
+
+   
+    public Aluno() {
+        super();
+    }
+
+    
+
+
+    public Aluno(String cursos, double[] notas) {
+        this.cursos = cursos;
+        this.notas = notas;
+    }
+
+
+
+
+    public Aluno(String nome, String telefone, String telefoneCelular, String cursos, double[] notas, String endereco) {
+        super(nome, telefone, telefoneCelular);
+        this.cursos = cursos;
+        this.notas = notas;
+    }
+
+
+
+
+    /**
+     * @return String return the cursos
+     */
+    public String getCursos() {
+        return cursos;
+    }
+
+    /**
+     * @param cursos the cursos to set
+     */
+    public void setCursos(String cursos) {
+        this.cursos = cursos;
+    }
+
+    /**
+     * @return double[] return the notas
+     */
+    public double[] getNotas() {
+        return notas;
+    }
+
+    /**
+     * @param notas the notas to set
+     */
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+    public String obterEtiquetaEndereco(){
+
+        String s = "Endereço do Aluno: ";
+        s += super.getEndereco();
+
+        return s; 
+    }
+
+
+
+
+    @Override
+    public void imprimirEtiquetaEndereco() {
+
+        System.out.println("Imprimindo o endereço do Aluno");
+        System.out.println(this.obterEtiquetaEndereco());
+    }
+
+}
