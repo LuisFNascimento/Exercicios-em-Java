91bf22aec81c47dedc2993b54376b409adbce29b
Luís Fernando Nascimento <luisfernandonascimentopro@gmail.com>
2023-10-31 23:56:01 -0300

    Exception

---
+++ Exercicios/src/cursojava/aula50/Agenda.java
@@ -0,0 +1,31 @@
+package cursojava.aula50;
+
+public class Agenda {
+    
+    private Contato[] contatos;
+
+    public Agenda(){
+        contatos = new Contato[5];
+
+    public void adicionarContato(Contato c) {
+
+        for (int i=0; i<contatos.length; i++){
+            if (contatos[i] != null){
+                contatos[i] = c;
+            }
+        }
+
+        return false;
+
+    }
+
+   @Override
+    public String toString() {
+        String s = " ";
+        for (Contato c : contatos){
+            s += c.toString() + "\n"
+        }
+        return s;
+    }
+    }
+}
